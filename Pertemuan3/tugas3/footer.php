284a0e6fc83b0787c5f461bac7da7c956598be45
Alifahza30 <alifahzaaa30@gmail.com>
2023-03-13 17:15:13 +0700

    tugas3

---
+++ Pertemuan3/tugas3/footer.php
@@ -0,0 +1,7 @@
+
+    <footer class="text-center py-3 bg-light mt-5">
+        <p>Created by <a href="#">Alifah Zahro Dzakiyyah</a> &copy; <?php echo date('Y') ?></p>
+    </footer>
+</body>
+
+</html>
