ca83b80dcbe25166d288bbe4d5ff077ca4a92d09
Alifahza30 <alifahzaaa30@gmail.com>
2023-05-25 15:46:14 +0700

    update

---
+++ movie-app/database/seeders/GenresSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Genre;
+use GMP;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class GenresSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Genre::create([
+            'nama' => 'Fantasy',
+            'deskripsi' => 'imajinatif, diluar nalar',
+        ]);
+
+        Genre::create([
+            'nama' => 'Horror',
+            'deskripsi' => 'menyeramkan',
+        ]);
+
+        Genre::create([
+            'nama' => 'Romance',
+            'deskripsi' => 'percintaan',
+        ]);
+        
+        Genre::create([
+            'nama' => 'Komedi',
+            'deskripsi' => 'lucu, jenaka',
+        ]);
+
+        Genre::create([
+            'nama' => 'Action',
+            'deskripsi' => 'seru',
+        ]);
+    }
+}
