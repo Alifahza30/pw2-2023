3f3d24ecf7be7f9c40cdfd016312b250573d1056
Alifahza30 <alifahzaaa30@gmail.com>
2023-06-08 15:08:36 +0700

    update laravel

--- movie-app/routes/web.php
+++ movie-app/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,18 +17,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('/movies', [MovieController::class, 'index']);
-Route::get('/movies/create', [MovieController::class, 'create']);
-Route::post('/movies', [MovieController::class, 'store']);
-Route::delete('/movies/{movie}', [MovieController::class, 'destroy']);
+Route::resource('/movies', MovieController::class);
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/genres', 'App\Http\Controllers\GenreController@index');
+Route::get('/genres/create', [GenreController::class, 'create']);
+Route::post('/genres', [GenreController::class, 'store']);
+Route::delete('/genres/{genre}', [GenreController::class, 'destroy']);
 
 Route::get('/reviews', 'App\Http\Controllers\ReviewController@index');
-
+Route::get('/reviews/create', [ReviewController::class, 'create']);
+Route::post('/reviews', [ReviewController::class, 'store']);
+Route::delete('/reviews/{review}', [ReviewController::class, 'destroy']);
 
 
