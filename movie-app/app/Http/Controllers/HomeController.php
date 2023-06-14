68cca448ec685583cecc9bbea2d7ed70c846ca2b
Alifahza30 <alifahzaaa30@gmail.com>
2023-06-14 18:46:49 +0700

    laravel edit

--- movie-app/app/Http/Controllers/HomeController.php
+++ movie-app/app/Http/Controllers/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Genre;
 use App\Models\Movie;
+use App\Models\Review;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -12,5 +14,12 @@ class HomeController extends Controller
     {
         $movies = Movie::all();
         return view('welcome', compact('movies'));
+
+        $genres = Genre::all();
+        return view('welcome', compact('genres'));
+
+        $reviews = Review::all();
+        return view('welcome', compact('reviews'));
     }
+    
 }
